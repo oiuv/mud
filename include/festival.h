d3ba853aee6891e2c534e82ea0b64815c8a9de46
Xuefeng <i@oiuv.cn>
2018-05-13 10:16:30 +0800

    更新 .gitignore

--- include/festival.h
+++ include/festival.h
@@ -3,11 +3,11 @@
 #define __FDETDAY__
 
 //#define BEGIN_TIME		1430409600
-#define FETEMONTH		10
-#define BEGIN_DAY		1
-#define END_DAY			8
-#define FETEDAY			"国庆节"
-#define FESTIVAL		"festival/2017/国庆节"
+#define FETEMONTH		5
+#define BEGIN_DAY		13 
+#define END_DAY			13 
+#define FETEDAY			"母亲节"
+#define FESTIVAL		"festival/2018/母亲节"
 
 #endif
 
