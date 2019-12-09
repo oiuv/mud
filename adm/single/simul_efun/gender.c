40a6589e1af6fc60b16605b45f0bf32942dfd516
Xuefeng <i@chenxuefeng.cn>
2019-12-09 19:59:00 +0800

    pr v2020

--- adm/single/simul_efun/gender.c
+++ adm/single/simul_efun/gender.c
@@ -2,22 +2,22 @@
 
 string gender_self(string sex)
 {
-	switch(sex) {
-		case "女性": return "你";	break;
-		default: return "你";
-	}
+   return "你";
 }
 
 string gender_pronoun(string sex)
 {
-	switch(sex) {
-		case "中性神":	return "他";	break;
-		case "男性":	return "他";	break;
-		case "无性":    return "他";    break;
-		case "女性":	return "她";	break;
-		case "雄性":
-		case "雌性":	return "它";	break;
-		default: return "它";
-	}
+   switch (sex)
+   {
+   case "中性神":
+   case "男性":
+   case "无性":
+      return "他";
+   case "女性":
+      return "她";
+   case "雄性":
+   case "雌性":
+   default:
+      return "它";
+   }
 }
-
