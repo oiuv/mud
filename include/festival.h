8813c7c168b6df8d2172321e693ff70eb5e9049f
oiuv <i@oiuv.cn>
2020-01-15 03:09:14 +0000

    happy new year!

--- include/festival.h
+++ include/festival.h
@@ -3,10 +3,10 @@
 #define __FDETDAY__
 
 //#define BEGIN_TIME		1430409600
-#define FETEMONTH		2
-#define BEGIN_DAY		1
-#define END_DAY			10
+#define FETEMONTH		1
+#define BEGIN_DAY		24
+#define END_DAY			31
 #define FETEDAY			"´º½Ú"
-#define FESTIVAL		"festival/2019/´º½Ú"
+#define FESTIVAL		"festival/2020/´º½Ú"
 
 #endif
