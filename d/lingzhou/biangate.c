b2a41801a3db0c75b0a50f1484fb5a17bb4db795
mudren <i@Mud.ren>
2021-09-19 10:32:55 +0800

    ...

--- d/lingzhou/biangate.c
+++ d/lingzhou/biangate.c
@@ -6,29 +6,29 @@
 inherit ROOM;
 void create()
 {
-	set("short", "边门");
-	set("long", @LONG
+    set("short", "边门");
+    set("long", @LONG
 这里是皇宫的边门，供侍卫和内侍们出入之用，宫里所需的东西一
 般都由此运入，因为比较偏僻，守卫的卫士也不多。
 LONG );
-	set("exits", ([
-		"west"  : __DIR__"caifang",
-		"east"  : __DIR__"xiaolu3",
-	]));
-	set("no_fight",1);
-	set("objects", ([
-		__DIR__"npc/gongweishi" : 2,
-	]));
-	setup();
-        create_door("west" , "红漆宫门", "east" , DOOR_CLOSED);
+    set("exits", ([
+        // todo 完善环境
+        // "west"  : __DIR__"caifang",
+        "east"  : __DIR__"xiaolu3",
+    ]));
+    set("no_fight",1);
+    set("objects", ([
+        __DIR__"npc/gongweishi" : 2,
+    ]));
+    setup();
+    // create_door("west" , "红漆宫门", "east" , DOOR_CLOSED);
 }
 
 int valid_leave(object me, string dir)
 {
-        if (dir == "west" && 
-            objectp(present("wei shi", environment(me))))
-           return notify_fail("卫士对你大吼一声：放肆！那不是你能进去的地方。\n");
+    if (dir == "west" &&
+        objectp(present("wei shi", environment(me))))
+        return notify_fail("卫士对你大吼一声：放肆！那不是你能进去的地方。\n");
 
-        return ::valid_leave(me, dir);
+    return ::valid_leave(me, dir);
 }
-
