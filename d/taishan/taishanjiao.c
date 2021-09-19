96d46db1a6c0dca7aadb3a0b859162098d158ec3
mudren <i@Mud.ren>
2021-09-19 10:37:51 +0800

    ...

--- d/taishan/taishanjiao.c
+++ d/taishan/taishanjiao.c
@@ -5,18 +5,18 @@ inherit ROOM;
 
 void create()
 {
-	set("short", "泰山脚下");
-	set("long", @LONG
-泰山古称岱山，为五岳的东岳，为道教第二小洞天，向有 \"五岳独
-尊\" 之称誉，以 \"泰山天下雄\" 名闻域内。往北就到岱宗坊了。
+    set("short", "泰山脚下");
+    set("long", @LONG
+泰山古称岱山，为五岳的东岳，为道教第二小洞天，向有『五岳独尊』
+之称誉，以『泰山天下雄』名闻域内。往北就到岱宗坊了。
 LONG );
-	set("exits", ([
-		"north" : __DIR__"daizong",
-		"south" : __DIR__"yidao3",
-                "east"  : __DIR__"kedian",
-	]));
-	set("no_clean_up", 0);
-	set("outdoors", "taishan");
-	setup();
-	replace_program(ROOM);
+    set("exits", ([
+        "north" : __DIR__"daizong",
+        "south" : __DIR__"yidao3",
+        "east"  : __DIR__"kedian",
+    ]));
+    set("no_clean_up", 0);
+    set("outdoors", "taishan");
+    setup();
+    replace_program(ROOM);
 }
