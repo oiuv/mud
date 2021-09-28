2d604a5ebc30f7ca80fced26d5fcecdd81b8c642
oiuv <i@oiuv.cn>
2021-09-28 20:02:59 +0800

    更新存储，统一no_put

--- feature/name.c
+++ feature/name.c
@@ -1,105 +1,108 @@
 // name.c
 
-#include <ansi.h>
-#include <dbase.h>
-#include <condition.h>
-
 nosave string *my_id;
 
 varargs void set_name(string name, string *id)
 {
-        string fullname;
+    string fullname;
 
-        if (! stringp(name))
+    if (!stringp(name))
+    {
+        if (!stringp(fullname = this_object()->query("surname")))
+            fullname = "";
+
+        if (stringp(this_object()->query("purename")))
+            fullname += this_object()->query("purename");
+
+        if (fullname == "")
+            fullname = "无名氏";
+
+        this_object()->set("name", fullname);
+    }
+    else
+    {
+        this_object()->set("name", name);
+    }
+
+    if (pointerp(id))
+    {
+        this_object()->set("id", id[0]);
+        my_id = id;
+        // 非玩家对象增加首字母ID，不可以使用 userp() 判断
+        if (!this_object()->is_user())
         {
-                if (! stringp(fullname = query("surname")))
-                        fullname = "";
-
-                if (stringp(query("purename")))
-                        fullname += query("purename");
-
-                if (fullname == "")
-                        fullname = "无名氏";
-
-                set("name", fullname);
-        } else
-        {
-	        set("name", name);
+            my_id += ({lower_case(id[0][0..0])});
         }
-
-	if (pointerp(id))
-        {
-                set("id", id[0]);
-	        my_id = id;
-        }
+    }
 }
 
 int id(string str)
 {
-//	string *applied_id;
+    //    string *applied_id;
 
-        if (! str) return 0;
-	if (this_object()->is_character() && this_player() &&
-	    ! this_player()->visible(this_object())) return 0;
+    if (!str)
+        return 0;
 
-	// Remove apply/id for speed
-	// If apply/id exists, this object is "pretending" something, don't
-	// recognize original id to prevent breaking the pretending with "id"
-	// command.
+    // 彻底隐身
+    if (this_object()->is_character() && this_player() &&
+        !this_player()->visible(this_object()))
+        return 0;
 
-	if (my_id && member_array(str, my_id) != -1)
-		return 1;
-	else
-		return 0;
+    // Remove apply/id for speed
+    // If apply/id exists, this object is "pretending" something, don't
+    // recognize original id to prevent breaking the pretending with "id"
+    // command.
+
+    if (my_id && member_array(str, my_id) != -1)
+        return 1;
+    else
+        return 0;
 }
 
 string *parse_command_id_list()
 {
-	string *applied_id;
+    string *applied_id;
 
-	if( pointerp(applied_id = query_temp("apply/id"))
-	&&	sizeof(applied_id) )
-		return applied_id;
-	else
-		return my_id;
+    if (pointerp(applied_id = this_object()->query_temp("apply/id")) && sizeof(applied_id))
+        return applied_id;
+    else
+        return my_id;
 }
 
 varargs string name(int raw)
 {
-	string str, *mask;
-
-	if (! raw && sizeof(mask = query_temp("apply/name")))
-		return mask[sizeof(mask)-1];
-	else
-        {
-		if (stringp(str = query("name")))
-			return str;
-		else
-			return file_name(this_object());
-	}
+    string str, *mask;
+    if (!raw && sizeof(mask = this_object()->query_temp("apply/name")))
+        return mask[sizeof(mask) - 1];
+    else
+    {
+        if (stringp(str = this_object()->query("name")))
+            return str;
+        else
+            return file_name(this_object());
+    }
 }
 
 varargs string short(int raw)
 {
-	string str;
+    string str;
 
-	if (raw || ! stringp(str = query("short")))
-		str = name(raw) + "(" + capitalize(query("id")) + ")";
-        return str;
+    if (raw || !stringp(str = this_object()->query("short")))
+        str = name(raw) + (stringp(this_object()->query("id")) ? "(" + capitalize(this_object()->query("id")) + ")" : "");
+    return str;
 }
 
 varargs string long(int raw)
 {
-	string str, extra, *mask;
+    string str, extra, *mask;
 
-	if (! raw && sizeof(mask = query_temp("apply/long")))
-		str = mask[sizeof(mask)-1];
-	else
-        if (! stringp(str = query("long")))
-		str = short(raw) + "。\n";
+    if (!raw && sizeof(mask = this_object()->query_temp("apply/long")))
+        str = mask[sizeof(mask) - 1];
+    else if (!stringp(str = this_object()->query("long")))
+        str = short(raw) + "。\n";
 
-	if (stringp(extra = this_object()->extra_long()))
-		str += extra;
+    if (stringp(extra = this_object()->extra_long()))
+        str += extra;
 
-	return str;
+    return str;
 }
