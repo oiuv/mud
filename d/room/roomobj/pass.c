82809b2b6ad384d4a826b5272e0f56688ba7082b
oiuv <i@oiuv.cn>
2021-10-21 19:56:22 +0800

    ...

--- d/room/roomobj/pass.c
+++ d/room/roomobj/pass.c
@@ -6,15 +6,17 @@ inherit ITEM;
 
 void create()
 {
-    set_name(YEL "手谕" NOR, ({ "pass" }) );
-        set_weight(10);
-    if( clonep() )
+    set_name(YEL "手谕" NOR, ({"pass"}));
+    set_weight(10);
+    if (clonep())
         set_default_object(__FILE__);
-    else {
+    else
+    {
         set("unit", "张");
-                set("no_sell", 1);
+        set("no_sell", 1);
+        set("no_put", 1);
         set("material", "paper");
     }
-        set("long", "一张手谕。\n");
+    set("long", "一张手谕。\n");
     setup();
 }
