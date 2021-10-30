67c8355170b5daf926b73bd88c3ffdf119190fc2
oiuv <i@oiuv.cn>
2021-10-30 15:55:24 +0800

    调整地图

--- d/jingzhou/road2.c
+++ d/jingzhou/road2.c
@@ -2,20 +2,19 @@ inherit ROOM;
 
 void create()
 {
-        set("short", "大道");
-        set("long", @LONG
+    set("short", "大道");
+    set("long", @LONG
 这是一条青石大道。东面隐约可以看到扬州城的城墙。向
 西看去，只见群山起伏，青翠碧绿，不知通向哪里。
 LONG);
-        set("exits", ([
-                "northeast" : __DIR__"road1",
-                "southwest" : __DIR__"road3",
-        ]));
+    set("exits", ([
+        "northeast" : __DIR__"road1",
+        "southwest" : __DIR__"road3",
+    ]));
 
-        set("no_clean_up", 0);
-        set("outdoors", "city");
+    set("no_clean_up", 0);
+    set("outdoors", "jingzhou");
 
-        setup();
-        replace_program(ROOM);
+    setup();
+    replace_program(ROOM);
 }
-
--- d/jingzhou/road3.c
+++ d/jingzhou/road3.c
@@ -2,20 +2,19 @@ inherit ROOM;
 
 void create()
 {
-        set("short", "大道");
-        set("long", @LONG
+    set("short", "大道");
+    set("long", @LONG
 这是一条青石大道。向东通往扬州。向西而去，只见一座
 高山青松古柏，一片大好风景。
 LONG);
-        set("exits", ([
-                "northeast" : __DIR__"road2",
-                "southwest" : __DIR__"road4",
-        ]));
+    set("exits", ([
+        "northeast" : __DIR__"road2",
+        "southwest" : __DIR__"road4",
+    ]));
 
-        set("no_clean_up", 0);
-        set("outdoors", "city");
+    set("no_clean_up", 0);
+    set("outdoors", "jingzhou");
 
-        setup();
-        replace_program(ROOM);
+    setup();
+    replace_program(ROOM);
 }
-
