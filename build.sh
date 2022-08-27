401014e61d715ede1b0a0cd02e4a8daaa30c408a
oiuv <i@oiuv.cn>
2022-08-27 18:51:17 +0800

    优化驱动编译脚本

--- build.sh
+++ build.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-cd fluffos || (git clone https://gitee.com/mudren/fluffos.git && cd fluffos)
-
-git pull
-rm -rf build && mkdir build && cd build
+if [ ! -d "fluffos" ]; then
+    git clone https://gitee.com/mudren/fluffos.git
+fi
+cd fluffos && git pull
+if [ -d "build" ]; then
+    rm -rf build
+fi
+mkdir build && cd build
 cmake -DPACKAGE_DB_MYSQL=1 -DPACKAGE_DB_SQLITE=2 .. && make -j4 install
