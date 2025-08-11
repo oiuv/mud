3ae07e10cd623331e5124a6f0f557e56d3c0d545
Oiuv <i@oiuv.cn>
2025-08-11 12:31:09 +0800

    添加MSP、MSDP和ZMP协议支持功能

--- feature/user_mxp.c
+++ feature/user_mxp.c
@@ -12,3 +12,41 @@ void act_mxp()
 {
     efun::act_mxp();
 }
+
+void msp_enable()
+{
+    receive("<MSP negotiation: enabled.>\n");
+}
+
+void msdp_enable()
+{
+    receive("<MSDP negotiation: enabled.>\n");
+}
+
+void msdp(string req)
+{
+    receive("<MSDP request received.>\n");
+    receive(req);
+    receive("\n<MSDP request end.>\n");
+}
+
+void send_msdp_variable(string name, mixed value)
+{
+    efun::send_msdp_variable(name, value);
+}
+
+void zmp_enable()
+{
+    receive("<ZMP negotiation: enabled.>\n");
+}
+
+// 收到ZMP命令时调用
+void zmp_command(string module, string *args)
+{
+    // todo
+}
+
+void send_zmp(string module, string *args)
+{
+    efun::send_zmp(module, args);
+}
