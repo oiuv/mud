9e1972e1a432a56ff8ff9d22891dea6c3fd38daf
Oiuv <i@oiuv.cn>
2025-09-13 23:37:52 +0800

    fix: 关闭手机短信接口并移除娱乐NPC神算子

--- adm/daemons/sms_d.c
+++ adm/daemons/sms_d.c
@@ -1,3 +1,9 @@
 // SMS 短信接口
 
 inherit CORE_DIR "system/daemons/http/sms_d";
+
+// 重写接口
+void sms(object me, mixed code, mixed mobile)
+{
+    tell_object(me, "【提示】短信服务关闭，暂不支持收发手机短信，请直接用验证码[" + code + "]绑定手机。\n");
+}
